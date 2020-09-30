87908862af6fd619f8f7d633f80611d0ae03d97d
Sakshigoyal2001 <72159388+Sakshigoyal2001@users.noreply.github.com>
2020-09-30 23:29:13 +0530

    Create pattern.py

---
+++ pattern.py
@@ -0,0 +1,8 @@
+for i in range (1,6):
+    for j in range(i):
+        print("*", end=' ')
+    print("\r")
+for i in range (5, 1, -1):
+    for j in range(0, i-1):
+        print("*", end=' ')
+    print("\r")
